1bf9f082c4b7a00256341361759393dfca20f344
Niti T <77049172+Sun-G-S6@users.noreply.github.com>
2023-09-03 23:28:50 -0700

    lab 1.16

--- lab1-15.c
+++ lab1-15.c
@@ -1,3 +1,12 @@
+/*
+1.15 LAB: Formatted output: Hello World!
+Write a program that outputs "Hello World!" as shown below. For ALL labs, end with newline (unless otherwise stated).
+
+CS 149 Section 2 Operating Systems
+Nitisuk Tatiyasuntorn
+Professor William "Bill" Andreopoulos
+*/
+
 #include <stdio.h>
 
 int main(void)
---
+++ lab1-16.c
@@ -0,0 +1,21 @@
+/*
+1.16 LAB: Formatted output: No parking sign
+Write a program that prints a formatted "No parking" sign as shown below. Note the first line has two leading spaces. For ALL labs, end with newline (unless otherwise stated).
+
+  NO PARKING
+2:00 - 6:00 a.m.
+
+CS 149 Section 2 Operating Systems
+Nitisuk Tatiyasuntorn
+Professor William "Bill" Andreopoulos
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+
+    printf("  NO PARKING\n2:00 - 6:00 a.m.\n");
+
+    return 0;
+}
